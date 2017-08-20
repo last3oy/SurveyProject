b6f6b6e5a023cf0dc7e0ea656fbf4b7376bd16d0
Patipon Suksangeam <softwarez_gelion@hotmail.com>
2017-08-20 22:13:23 +0700

    implement databinding in SurveyDetailActivity

--- app/src/main/java/com/example/last3oy/surveyproject/activity/surveydetail/view/SurveyDetailActivity.java
+++ app/src/main/java/com/example/last3oy/surveyproject/activity/surveydetail/view/SurveyDetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.last3oy.surveyproject.activity.surveydetail.view;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,16 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import com.example.last3oy.surveyproject.R;
 import com.example.last3oy.surveyproject.activity.surveydetail.SurveyDetailContact;
 import com.example.last3oy.surveyproject.activity.surveydetail.presenter.SurveyDetailPresenter;
+import com.example.last3oy.surveyproject.databinding.ActivitySurveyDetailBinding;
 
 public class SurveyDetailActivity extends AppCompatActivity implements SurveyDetailContact.View {
 
+    private ActivitySurveyDetailBinding mBinding;
 
     private SurveyDetailContact.Presenter mPresenter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_survey_detail);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_survey_detail);
 
         mPresenter = new SurveyDetailPresenter(this);
     }
